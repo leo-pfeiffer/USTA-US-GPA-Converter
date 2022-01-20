478fb3a5636f75c337ef1c0a0b34615f2e61c912
Leopold Pfeiffer <50983452+leo-pfeiffer@users.noreply.github.com>
2022-01-20 14:18:25 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <img alt="Vue logo" src="./assets/crest.png" />
+  <HelloWorld
+    :msg="'University of St Andrews degree classification to US GPA conversion'"
+    :conversion="[
+      {
+        class: '1st class',
+        stALB: 17.5,
+        stAUB: 20,
+        USLB: 4.0,
+        USUB: 4.0,
+      },
+      {
+        class: '1st class',
+        stALB: 16,
+        stAUB: 17.4,
+        USLB: 3.7,
+        USUB: 3.9,
+      },
+      {
+        class: '2nd class (division one)',
+        stALB: 13,
+        stAUB: 16.4,
+        USLB: 3,
+        USUB: 3.6,
+      },
+      {
+        class: '2nd class (division two)',
+        stALB: 10,
+        stAUB: 13.4,
+        USLB: 2.3,
+        USUB: 2.9,
+      },
+      {
+        class: '3rd class',
+        stALB: 7,
+        stAUB: 10.4,
+        USLB: 1.3,
+        USUB: 2.2,
+      },
+    ]"
+  />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+  data: function () {
+    return {
+      conversion: [
+        {
+          hello: "first_name",
+          showname: "First Name",
+          type: "text",
+          required: "false",
+          fee: "0",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  max-width: 30%;
+  min-width: 75px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      The conversion scale as provided by the University of St Andrews can be
+      found
+      <a
+        href="https://www.st-andrews.ac.uk/administration/academicdatateam/assessmentandawards/undergraduateinformation/"
+        target="_blank"
+        rel="noopener"
+        >here</a
+      >.
+    </p>
+    <table>
+      <thead>
+        <col />
+        <colgroup span="2"></colgroup>
+        <colgroup span="2"></colgroup>
+        <tr>
+          <th></th>
+          <th colspan="2" scope="colgroup">St Andrews GPA range</th>
+          <th colspan="2" scope="colgroup">US GPA range</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <thead>
+        <tr>
+          <th>Classification</th>
+          <th>from</th>
+          <th>to</th>
+          <th>from</th>
+          <th>to</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="c in conversion" v-bind:key="c.class">
+          <td>{{ c.class }}</td>
+          <td>{{ c.stALB }}</td>
+          <td>{{ c.stAUB }}</td>
+          <td>{{ c.USLB }}</td>
+          <td>{{ c.USUB }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <hr />
+    <p>Please enter your St Andrews GPA:</p>
+    <input
+      v-model="staGPA"
+      @input="validateInput"
+      type="number"
+      placeholder="St Andrews GPA"
+    />
+    <p v-if="state === 'show'">
+      Your St Andrews GPA of {{ staGPA }} corresponds to a US GPA of
+      {{ round(convertToUS(Number(staGPA))) }}.
+    </p>
+    <p v-if="state === 'nan'">Please enter a valid GPA between 7 and 20.</p>
+    <p v-if="state === 'lowGPA'">
+      Your GPA must be higher than 7 for the conversion.
+    </p>
+    <p v-if="state === 'highGPA'">Your GPA cannot exceed 20.</p>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    conversion: Array,
+  },
+  methods: {
+    validateInput() {
+      console.log(this.staGPA);
+      if (this.staGPA === "") {
+        this.state = "hidden";
+      } else if (isNaN(this.staGPA)) {
+        this.state = "nan";
+      } else {
+        this.staGPA = Number(this.staGPA);
+        if (this.staGPA < 7) {
+          this.state = "lowGPA";
+        } else if (this.staGPA > 20) {
+          this.state = "highGPA";
+        } else {
+          this.state = "show";
+        }
+      }
+      return;
+    },
+    convertToUS(gpa) {
+      if (isNaN(gpa)) {
+        return;
+      }
+      let idx = this.getConversionIndex(gpa);
+      if (idx === -1) {
+        return;
+      }
+      let stARange = this.calcStAndrewsRange(idx);
+      let usRange = this.calcUSRange(idx);
+      let dist = gpa - this.conversion[idx].stALB;
+      let quotient = dist / stARange;
+
+      return this.conversion[idx].USLB + quotient * usRange;
+    },
+    getConversionIndex(gpa) {
+      for (let c in this.conversion) {
+        let cls = this.conversion[c];
+        if (cls.stALB <= gpa && gpa <= cls.stAUB) {
+          return c;
+        }
+      }
+      return -1;
+    },
+    calcUSRange(idx) {
+      let lb = this.conversion[idx].USLB;
+      let ub = this.conversion[idx].USUB;
+      return this.calcRange(lb, ub);
+    },
+    calcStAndrewsRange(idx) {
+      console.log(idx);
+      console.log(this.conversion[idx]);
+      let lb = this.conversion[idx].stALB;
+      let ub = this.conversion[idx].stAUB;
+      return this.calcRange(lb, ub);
+    },
+    calcRange(lb, ub) {
+      return ub - lb;
+    },
+    round(num) {
+      return Math.round((num + Number.EPSILON) * 100) / 100;
+    },
+  },
+  data: function () {
+    return {
+      staGPA: "",
+      state: "hidden",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+table {
+  display: inline-block;
+  margin: 0 10px;
+  text-align: right;
+}
+th,
+td {
+  padding: 0 15px;
+}
+a {
+  color: #42b983;
+}
+</style>
